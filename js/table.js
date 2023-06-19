b9aa3415c0f83f1a76386870760bcbf151ff620a
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2023-06-19 21:05:58 +0530

    fix : snapshot-2

--- js/table.js
+++ js/table.js
@@ -1,20 +1,26 @@
-var timelineSwiper = new Swiper(".timeline .swiper-container", {
-  direction: "vertical",
-  loop: false,
-  speed: 1600,
-  pagination: ".swiper-pagination",
-  paginationBulletRender: function (swiper, index, className) {
-    var year = document
-      .querySelectorAll(".swiper-slide")
-      [index].getAttribute("data-year");
-    return '<span class="' + className + '">' + year + "</span>";
-  },
-  paginationClickable: true,
-  nextButton: ".swiper-button-next",
-  prevButton: ".swiper-button-prev",
-  breakpoints: {
-    768: {
-      direction: "horizontal",
-    },
-  },
+function updateImageContainerMargin() {
+  var navHeight = document.querySelector(".navigation-wrap").offsetHeight;
+  var imageContainer = document.querySelector(".panels");
+  imageContainer.style.marginTop = navHeight + "px";
+}
+var navHeight = document.querySelector(".navigation-wrap").offsetHeight;
+var imageContainer = document.querySelector(".panels");
+imageContainer.style.marginTop = navHeight + "px";
+// Call the function initially
+updateImageContainerMargin();
+
+// Call the function on window resize
+window.addEventListener("resize", updateImageContainerMargin);
+
+// Select all elements with the class "my-class"
+const elements = document.querySelectorAll(".hydro-disabled");
+
+// Add a click event listener to each element
+elements.forEach((element) => {
+  element.addEventListener("click", () => {
+    // Handle the click event for each element
+    console.log("Element clicked:", element);
+    alert("Sorry for the inconvenience, That feature is still WIP");
+    // Add your desired actions or behavior here
+  });
 });
